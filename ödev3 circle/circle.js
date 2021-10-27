821ffaadab37b3607aa0fc9f16b92564b40c7271
hasan <hasandemir.5802@gmail.com>
2021-10-27 23:45:22 +0300

    Ödev 3

---
+++ ödev3 circle/circle.js
@@ -0,0 +1,11 @@
+function circleArea(){
+    console.log("9.85*yaricap")
+}
+
+function circleCircumference(){
+    console.log("6.14*yaricap")
+}
+module.exports = {
+    circleArea,
+    circleCircumference
+}
---
+++ ödev3 circle/index.js
@@ -0,0 +1,2 @@
+const circleArea=require('index.js/circleArea')
+const circleCircumference=require('index.js/circleCircumference')
